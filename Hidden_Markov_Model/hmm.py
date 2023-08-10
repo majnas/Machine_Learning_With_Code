668d7604b0702f5cb5f9c4c352873ce008e14f7a
Majid Nasiri <majnasai@gmail.com>
2023-08-10 17:34:45 +1000

    add doc to HMM Class

--- Hidden_Markov_Model/hmm.py
+++ Hidden_Markov_Model/hmm.py
@@ -3,7 +3,26 @@ from typing import List
 import numpy as np
 
 class HMM:
+    """
+    Hidden Markov Model (HMM) for image classification using angle differences.
+
+    Attributes:
+        n_state (int): Number of states (classes) in the HMM.
+        n_emission (int): Number of possible emissions (angle differences) in the HMM.
+        emission (dict): Emission probabilities for each state and emission.
+        pi (dict): Prior probabilities for each state.
+        transition (dict): Transition probabilities between states.
+
+    Methods:
+        __init__(n_state, n_emission): Initialize the HMM with the specified number of states and emissions.
+        fit(samples): Train the HMM using the provided training samples.
+        predict(observations): Predict the state of a sequence of observations using the Viterbi algorithm.
+        save(path): Save the trained HMM model to a file.
+        load(path): Load a trained HMM model from a file.
+        __str__(): Return a string representation of the HMM's properties.
+    """
     def __init__(self, n_state: int, n_emission: int) -> None:
+        """Initialize the Hidden Markov Model."""
         self.n_state = n_state
         self.n_emission = n_emission
         self.emission = {}
@@ -15,6 +34,7 @@ class HMM:
             self.emission[state_idx] = {f"E{ne}":0 for ne in range(n_emission)}
 
     def fit(self, samples):
+        """Train the Hidden Markov Model using the provided training samples."""
         for sample in samples:
             observations = sample["observations"]
             for ob in observations:
@@ -26,6 +46,15 @@ class HMM:
 
 
     def predict(self, observations: List):
+        """
+        Predict the state of a sequence of observations using the Viterbi algorithm.
+
+        Args:
+            observations (list): List of observations (angle differences) for prediction.
+
+        Returns:
+            tuple: A tuple containing the predicted state and a dictionary of state votes.
+        """
         n_observation = len(observations)
         observation_probabilities = {-1: self.pi}
         for current_observation_idx, observation in enumerate(observations):
@@ -55,6 +84,12 @@ class HMM:
         return winner_class, state_vote
     
     def save(self, path: str = "model.npz"):
+        """
+        Save the trained Hidden Markov Model to a file.
+
+        Args:
+            path (str, optional): Path to save the model file. Defaults to "model.npz".
+        """
         if not path.endswith(".npz"):
             path += ".npz"
         with open(path, 'wb') as file:
@@ -66,6 +101,12 @@ class HMM:
                                 transition=self.transition)
 
     def load(self, path: str = "model.npz"):
+        """
+        Load a trained Hidden Markov Model from a file.
+
+        Args:
+            path (str, optional): Path to the model file. Defaults to "model.npz".
+        """
         with open(path, 'rb') as file:
             data = np.load(file, allow_pickle=True)
             n_state = data["n_state"]
@@ -83,6 +124,7 @@ class HMM:
 
 
     def __str__(self) -> str:
+        """Return a string representation of the Hidden Markov Model's properties."""
         msg = f"HMM Properties:\n"
         msg += f"n_state: {self.n_state}\n"
         msg += f"n_emission: {self.n_emission}\n"
