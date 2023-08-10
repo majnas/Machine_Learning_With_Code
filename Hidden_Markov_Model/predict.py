017d23354b16cdc15683e38295e166ac370a4645
Majid Nasiri <majnasai@gmail.com>
2023-08-10 17:38:44 +1000

    add doc

--- Hidden_Markov_Model/predict.py
+++ Hidden_Markov_Model/predict.py
@@ -10,6 +10,15 @@ from hmm import HMM
 
 
 def main(args):
+    """
+    Main function to predict the class of an image using a trained Hidden Markov Model (HMM).
+
+    Args:
+        args (argparse.Namespace): Command-line arguments.
+
+    Returns:
+        None
+    """
     with open(args.cfg, "r") as f:
         config_data = yaml.load(f, Loader=yaml.FullLoader)
     config = utils.Config(**config_data)
@@ -17,16 +26,18 @@ def main(args):
     #? Instantiate preprocessing object
     pp = utils.Preprocess(n_emission=config.n_emission)
 
+    #? Load the trained HMM model
     hmm_obj = HMM(n_state=config.n_state, n_emission=config.n_emission)
     hmm_obj.load()
     print(hmm_obj)
 
+    #? Get a sample image and preprocess it
     status, sample = create_dataset.get_sample(img_path=args.img_path, n_observations=config.n_observations)
     if status:
         sample = pp.preprocess_single_sample(sample=sample)
         winner_class, state_vote = hmm_obj.predict(observations=sample["observations"])
 
-        # Visualize image and prediction
+        #? Visualize image and prediction
         img = cv2.imread(args.img_path)
         cv2.putText(img, f"Prediction: {config.classes[winner_class]}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 2)
         cv2.putText(img, f"State_vote: {state_vote}", (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 2)
@@ -39,8 +50,8 @@ def main(args):
         cv2.destroyAllWindows()
 
 
-
 if __name__ == "__main__":
+    #? Command-line argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='config.yaml', help='config.yaml path')
     parser.add_argument('-i', '--img_path', type=str, default='./dataset/test/square/non_ideal_square_0.png', help='path to circle to square png image')
