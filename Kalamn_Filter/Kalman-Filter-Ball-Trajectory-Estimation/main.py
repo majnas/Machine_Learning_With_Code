51c6bae7827fd1db823227a4acdd30b30d344954
Majid Nasiri <majnasai@gmail.com>
2023-08-16 23:26:22 +1000

    redecorate

--- Kalamn_Filter/Kalman-Filter-Ball-Trajectory-Estimation/main.py
+++ Kalamn_Filter/Kalman-Filter-Ball-Trajectory-Estimation/main.py
@@ -4,7 +4,8 @@ from filterpy.kalman import KalmanFilter
 import matplotlib.pyplot as plt
 
 
-def get_kf(x, noise_std):
+# Define a function to initialize the Kalman Filter
+def get_kf(x, dt, noise_std):
     # Kalman filter setup
     kf = KalmanFilter(dim_x=4, dim_z=2)
     kf.x = np.array(x).reshape(4,1)  # Initial state [x, vx, y, vy]
@@ -26,80 +27,84 @@ def get_kf(x, noise_std):
                      [0, 0, 1, 0]])
     return kf
 
+def main():
+    # Constants
+    gx = 0
+    gy = -9.8  # Acceleration due to gravity
 
-# Constants
-gx = 0
-gy = -9.8  # Acceleration due to gravity
+    # Initial conditions
+    x0 = 0
+    y0 = 30
+    v0 = 20  # Initial velocity
+    alpha = np.radians(60)  # Shooting angle in radians
+    vx0 = v0 * math.cos(alpha)
+    vy0 = v0 * math.sin(alpha)
 
-# Initial conditions
-x0 = 0
-y0 = 30
-v0 = 20  # Initial velocity
-alpha = np.radians(60)  # Shooting angle in radians
-vx0 = v0 * math.cos(alpha)
-vy0 = v0 * math.sin(alpha)
+    # Time settings
+    t_start = 0
+    t_end = 10
+    n_steps = 30
+    dt = (t_end - t_start) / n_steps  # Time step
+    t = np.linspace(t_start, t_end, n_steps)
 
-# Time settings
-t_start = 0
-t_end = 10
-n_steps = 30
-dt = (t_end - t_start) / n_steps  # Time step
-t = np.linspace(t_start, t_end, n_steps)
+    # Generate ground truth trajectory
+    x = x0 + vx0 * t + 0.5 * gx * t * t
+    y = y0 + vy0 * t + 0.5 * gy * t * t
 
-# Generate ground truth trajectory
-x = x0 + vx0 * t + 0.5 * gx * t * t
-y = y0 + vy0 * t + 0.5 * gy * t * t
+    # Add random noise to x and y to simulate measurements
+    np.random.seed(0)  # For reproducibility
+    noise_std = 1.0
+    x_obs = x + np.random.normal(0, noise_std, n_steps)
+    y_obs = y + np.random.normal(0, noise_std, n_steps)
 
-# Add random noise to x and y to simulate measurements
-np.random.seed(0)  # For reproducibility
-noise_std = 1.0
-x_obs = x + np.random.normal(0, noise_std, n_steps)
-y_obs = y + np.random.normal(0, noise_std, n_steps)
+    kf1 = get_kf([x0, vx0, y0, vy0], dt, noise_std)
+    kf2 = get_kf([x0, vx0, y0, vy0], dt, noise_std)
 
-kf1 = get_kf([x0, vx0, y0, vy0], noise_std)
-kf2 = get_kf([x0, vx0, y0, vy0], noise_std)
+    # Perform Kalman filtering
+    filtered_state_means_1 = []
+    filtered_state_means_2 = []
+    u = np.array([[gx], 
+                  [gy]]).reshape(2,1)
 
-# Perform Kalman filtering
-filtered_state_means_1 = []
-filtered_state_means_2 = []
-u = np.array([[gx], 
-              [gy]]).reshape(2,1)
+    B = np.array([[0.5*dt*dt, 0],
+                [dt,        0],
+                [0, 0.5*dt*dt],
+                [0,        dt]]).reshape(4,2)
+    for obs in zip(x_obs, y_obs):
+        kf1.predict()
+        kf1.update(obs)
+        filtered_state_means_1.append(kf1.x)
 
-B = np.array([[0.5*dt*dt, 0],
-              [dt,        0],
-              [0, 0.5*dt*dt],
-              [0,        dt]]).reshape(4,2)
-for obs in zip(x_obs, y_obs):
-    kf1.predict()
-    kf1.update(obs)
-    filtered_state_means_1.append(kf1.x)
+        kf2.predict(u=u, B=B)
+        kf2.update(obs)
+        filtered_state_means_2.append(kf2.x)
 
-    kf2.predict(u=u, B=B)
-    kf2.update(obs)
-    filtered_state_means_2.append(kf2.x)
+    filtered_state_means_1 = np.array(filtered_state_means_1)
+    filtered_state_means_2 = np.array(filtered_state_means_2)
 
-filtered_state_means_1 = np.array(filtered_state_means_1)
-filtered_state_means_2 = np.array(filtered_state_means_2)
+    # Plotting the results
+    plt.figure(figsize=(10, 6))
 
-# Plotting the results
-plt.figure(figsize=(10, 6))
+    # Plot Ground Truth
+    plt.plot(x, y, label='Ground Truth', color='black')
 
-# Plot Ground Truth
-plt.plot(x, y, label='Ground Truth', color='red')
+    # Plot Noisy Measurements
+    plt.plot(x_obs, y_obs, label='Noisy Measurements', marker='o', linestyle='None', color='red')
 
-# Plot Noisy Measurements
-plt.plot(x_obs, y_obs, label='Noisy Measurements', marker='o', linestyle='None')
+    # Plot Filtered Estimate 1
+    plt.plot(filtered_state_means_1[:, 0], filtered_state_means_1[:, 2], label='Filtered Estimate 1', marker='o', color='green')
 
-# Plot Filtered Estimate 1
-plt.plot(filtered_state_means_1[:, 0], filtered_state_means_1[:, 2], label='Filtered Estimate 1', marker='o', color='green')
+    # Plot Filtered Estimate 2
+    plt.plot(filtered_state_means_2[:, 0], filtered_state_means_2[:, 2], label='Filtered Estimate 2', marker='o', color='blue')
 
-# Plot Filtered Estimate 2
-plt.plot(filtered_state_means_2[:, 0], filtered_state_means_2[:, 2], label='Filtered Estimate 2', marker='o', color='blue')
+    # Add labels and legend
+    plt.legend()
+    plt.title('Kalman Filter for Ball Trajectory Estimation with Noisy Observations')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(True)
+    plt.show()
 
-# Add labels and legend
-plt.legend()
-plt.title('Kalman Filter for Ball Trajectory Estimation with Noisy Observations')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.grid(True)
-plt.show()
+if __name__ == "__main__":
+    main()
+
